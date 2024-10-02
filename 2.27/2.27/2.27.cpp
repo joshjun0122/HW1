2ff1dea7136f2005b5b0df2fd9d4b52ed5b8f4ff
joshjun0122 <josh989870@gmail.com>
2024-10-02 19:42:50 +0800

    message

--- 2.27/2.27/2.27.cpp
+++ 2.27/2.27/2.27.cpp
@@ -1,16 +1,12 @@
 ﻿#include <iostream>
 #include <stdio.h>
 
-int x,y;
-
-int main()
-{
-    printf("請輸入your value : ");
-    scanf("%d,%d",&x,&y);
-    if ((x % y) == 0)
-        printf("是");
-    else
-        printf("否");
+int main() {
+    printf("\t    *    \n");
+    printf("\t   ***   \n");
+    printf("\t  *****  \n");
+    printf("\t ******* \n");
+    printf("\t*********\n");
 
     return 0;
 }
